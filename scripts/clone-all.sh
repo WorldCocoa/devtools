946ac3938edd4709d7292f213ee7d14abb7a7293
Thomas Gruner <tom.gruner@gmail.com>
2014-03-20 16:57:59 -0700

    Makefile

--- scripts/clone-all.sh
+++ scripts/clone-all.sh
@@ -1,9 +1,13 @@
 #/bin/bash
-for line in $(cat repository.list.txt); do
-	#directory_name
-	#Get the default directory name from the github url
-	#for example git@github.com:GlobAllomeTree/docker-elasticsearch.git becomes docker-elasticsearch.
-    directory_name=$(echo ${line} | sed 's:.*/::' | sed 's/\.git//')  
-    echo $directory_name
-    echo $line
+#working dirctory is were the Makefile is
+
+#Include our helpers file (the . is short for an include in bash )
+. scripts/helpers.sh
+
+for repository in $(cat repository.list.txt); do
+    dir_name=$(repository_directory_name ${repository})
+    #If the repository directory does not exist, then clone the repo
+    if [ ! -d ${dir_name} ]; then 
+    	git clone ${repository} ${dir_name}
+	fi
 done
