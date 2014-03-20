be6d8a4d1b7255a932654db7e894d3002f38dc49
Thomas Gruner <tom.gruner@gmail.com>
2014-03-20 13:29:20 -0700

    adding in some utility scripts

--- scripts/clone-all.sh
+++ scripts/clone-all.sh
@@ -1,4 +1,9 @@
 #/bin/bash
 for line in $(cat repository.list.txt); do
-    git clone ${line}   
+	#directory_name
+	#Get the default directory name from the github url
+	#for example git@github.com:GlobAllomeTree/docker-elasticsearch.git becomes docker-elasticsearch.
+    directory_name=$(echo ${line} | sed 's:.*/::' | sed 's/\.git//')  
+    echo $directory_name
+    echo $line
 done
